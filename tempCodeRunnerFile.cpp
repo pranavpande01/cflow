17ad718fbaeecc4ee1180b3911169b820645320e
unknown <93655463+pranavpande01@users.noreply.github.com>
2024-07-02 05:35:02 +0530

    third commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -7,12 +7,13 @@
 using namespace std;
 
 
-char eq[]="((1)+((x)^(4)))-(((2)*(x))-(3))";
-int len=sizeof(eq)/sizeof(char);
+char eq[]="(((1)+((x)^(4)))-(((2)*(x))-(3)))";
 
 
-
-
+struct node
+{
+    
+};
 
 class Stack {
     int top;
@@ -68,15 +69,14 @@ bool Stack::isEmpty()
     return (top < 0);
 }
 
-vector<vector<int>> pairs;
-void re(int index){
-    //cout<<len;
-    //cout<<eq[index];
+vector<vector<int>> re(int index){
+    
     Stack open;
     vector<int> temp;
     vector<vector<int>> temp_;
+    vector<vector<int>> pairs;
+    while(index<(sizeof(eq)/sizeof(char))){
 
-    while(index<len){
 
         if(eq[index]=='('){
             open.push(index);
@@ -84,9 +84,9 @@ void re(int index){
         else if (eq[index]==')')
         {
             temp={open.peek(),index};
-            cout<<open.peek()<<"-"<<index<<"\n";
+            //cout<<open.peek()<<"-"<<index<<"\n";
             temp_={temp};
-            pairs.insert(pairs.end(),temp_.begin(),temp_.end());
+            pairs.insert(pairs.end(),temp_.begin(),temp_.end());            
             open.pop();
         }
         else{
@@ -95,13 +95,15 @@ void re(int index){
         index++;
     
     }
+    return pairs;
 }
 
 int main()
 {
-    re(0);
-for(int i=0;i<(sizeof(pairs)/sizeof(pairs[0]));i++){
-    //cout<<pairs[i][0]<<"-"<<pairs[i][1]<<"\n";
+    vector<vector<int>> pairs=re(0);
+    int len=pairs.size();
+for(int i=0;i<len;i++){
+    cout<<pairs[i][0]<<"-"<<pairs[i][1]<<"\n";
     }
     return 0;
 }
